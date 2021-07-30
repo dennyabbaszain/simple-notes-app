c3f99ad8aab8aa65be0b97eee1c05ae4b1458781
dennyabbaszain <abbasdenny@gmail.com>
2021-07-30 10:24:43 +0700

    initial commit

--- src/container/pages/dashboard/index.js
+++ src/container/pages/dashboard/index.js
@@ -31,7 +31,7 @@ function Dashboard() {
       );
       return dataUserFirebase;
     })();
-  }, [dispatch, userData.uid]);
+  }, [dispatch, userData]);
 
   if (!dataGlobal.isLogin) history.push('/login');
 
